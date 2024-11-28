f5a4b811e9cd352c8517d70f41f42863e1a73f75
Natan Menegasse <natan.menegasse@hotmail.com>
2024-11-28 19:26:57 -0300

    Handling returns explicitly

--- source/main.c
+++ source/main.c
@@ -6,6 +6,7 @@
 #include <raylib.h>
 #include <time.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include "player.h"
 #include "game_state.h"
@@ -24,44 +25,53 @@ common_return_t
 rpg_game_setup(rpg_game_state_t *gs inout())
 {
     (void)gs;
+
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
+
     rl_set_config_flags(FLAG_MSAA_4X_HINT);
     rl_set_trace_log_level(LOG_NONE);
     rl_set_target_fps(FRAME_RATE);
-    return common_set_return(COMMON_OK, NULL, NULL);
+
+    return error;
 }
 
 common_return_t
 rpg_game_init(rpg_game_state_t *gs inout())
 {
-    common_set_defer();
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
 
     gs->time_frame = 0.0f;
+
+    if (access("something_wrong", F_OK) != 0) {
+        common_log(DEBUG, "File doesn't exists - Success");
+    }
 
     using_rpg_ui_config_t(gs, ui_config);
     unlikely_error(ui_init_set_config(ui_config)) {
         common_log(ERROR, ERROR_FMT, "initialize the UI config", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
 
     using_rpg_ui_state_t(gs, ui_state);
     unlikely_error(ui_state_init_config(ui_state)) {
         common_log(ERROR, ERROR_FMT, "initialize the UI state", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
 
     using_rpg_player_t(gs, player);
     unlikely_error(rpg_player_init(player)) {
         common_log(ERROR, ERROR_FMT, "initialize the player", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
 
     unlikely_error(rpg_player_update_display_info(player)) {
         common_log(ERROR, ERROR_FMT, "update display information during initialization",
                    common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
 
-    common_return;
+label_return:
+    return error;
 }
 
 common_return_t
@@ -71,7 +81,7 @@ rpg_game_logic_step(rpg_game_state_t *gs inout())
     assert(gs != NULL && "game state cannot be null");
 #endif
 
-    common_set_defer();
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
 
     using_rpg_player_t(gs, player);
 
@@ -100,18 +110,17 @@ rpg_game_logic_step(rpg_game_state_t *gs inout())
 
     mpf_clear(gain);
 
-    common_return;
+    return error;
 }
 
 common_return_t
 rpg_game_running(rpg_game_state_t *gs inout())
 {
-
 #ifdef DEVELOPER_BUILD
     assert(gs != NULL && "game state cannot be null");
 #endif
 
-    common_set_defer();
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
 
     using_rpg_player_t(gs, player);
     using_rpg_ui_config_t(gs, ui_config);
@@ -156,82 +165,83 @@ rpg_game_running(rpg_game_state_t *gs inout())
     }
     rl_close_window();
 
-    return common_set_return(COMMON_OK, NULL, NULL);
+    return error;
 }
 
 
 common_return_t
 rpg_game_deinit(rpg_game_state_t *gs inout())
 {
-
 #ifdef DEVELOPER_BUILD
     assert(gs != NULL && "game state cannot be null");
 #endif
 
     (void)gs;
-    return common_set_return(COMMON_OK, NULL, NULL);
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
+    return error;
 }
 
 common_return_t
 rpg_game_shutdown(rpg_game_state_t *gs inout())
 {
-
 #ifdef DEVELOPER_BUILD
     assert(gs != NULL && "game state cannot be null");
 #endif
 
     (void)gs;
-    return common_set_return(COMMON_OK, NULL, NULL);
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
+    return error;
 }
 
 common_return_t
 main_game(void)
 {
-    common_set_defer();
+    common_return_t error = common_set_return(COMMON_OK, NULL, NULL);
     rpg_game_state_t gs = {0};
 
     common_log(TRACE, "Setting up the game configuration");
     unlikely_error(rpg_game_setup(&gs)) {
         common_log(ERROR, ERROR_FMT, "setup the game", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
     common_log(TRACE, "Successfully configured the game");
 
     common_log(TRACE, "Initializing the game configuration");
     unlikely_error(rpg_game_init(&gs)) {
         common_log(ERROR, ERROR_FMT, "initialize the game", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
     common_log(TRACE, "Successfully initialized the game");
 
     common_log(TRACE, "Running the game");
     unlikely_error(rpg_game_running(&gs)) {
         common_log(ERROR, ERROR_FMT, "run the game", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
     common_log(TRACE, "Successfully run the game");
 
     common_log(TRACE, "De-initializing the game");
     unlikely_error(rpg_game_deinit(&gs)) {
         common_log(ERROR, ERROR_FMT, "de-initialize the game", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
     common_log(TRACE, "Successfully de-initialized the game");
 
     common_log(TRACE, "Shuttingdown the game");
     unlikely_error(rpg_game_shutdown(&gs)) {
         common_log(ERROR, ERROR_FMT, "shutdown the game", common_get_error_msg(error));
-        common_defer;
+        goto label_return;
     }
     common_log(TRACE, "Successfully shutdown the game");
 
-    common_return;
+label_return:
+    return error;
 }
 
 int
 main(void)
 {
-    common_return_t error = main_game();
-    return common_get_error(error) == COMMON_OK;
+    main_game();
+    return 0;
 }
 
