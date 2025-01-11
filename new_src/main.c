de9e40bc1419adbc785f143e3e1b36b6ecead2b6
Natan Menegasse <natan.menegasse@hotmail.com>
2025-01-10 23:58:06 -0300

    More improvements based on previous code

--- new_src/main.c
+++ new_src/main.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <gmp.h>
 
 #if DEVELOPER_BUILD
 #    include <assert.h>
@@ -10,47 +11,65 @@
 #define _In
 #define _Out
 
-#define type
-#define proc
-#define macro
-
-enum Result_e type {
+enum Result_e {
     OK,
     ERROR,
 };
 
-struct Result type {
+struct Result {
     enum Result_e res;
     char * _Nullable dev_error_msg;
     char * _Nullable user_error_msg;
 };
 
-#define set_result(res, dev_error_msg, user_error_msg) macro \
+#define set_result(res, dev_error_msg, user_error_msg) \
     (struct Result) { (res), (dev_error_msg), (user_error_msg) }
 
-#define get_result(Result)         macro (Result).res
-#define get_error(Result)          macro (Result).res
-#define get_dev_error_msg(Result)  macro (Result).dev_error_msg
-#define get_user_error_msg(Result) macro (Result).user_error_msg
+#define get_result(Result) (Result).res
+#define get_error(Result) (Result).res
+#define get_dev_error_msg(Result) (Result).dev_error_msg
+#define get_user_error_msg(Result) (Result).user_error_msg
 
+#define start_renderer(keep_rendering) while ((keep_rendering)) {
+#define end_renderer() }
 
-struct UI_Config type {
+#define new_mpf_from_struct(variable_name, initial_value) \
+    mpf_init((variable_name));                            \
+    mpf_set_str((variable_name), (initial_value), 10)
+
+#define new_mpf(variable_name, initial_value)         \
+    mpf_t (variable_name);                            \
+    mpf_init((variable_name));                        \
+    mpf_set_str((variable_name), (initial_value), 10)
+
+#define clear_mpf(variable_name)             \
+    mpf_set_str((variable_name), "0.0", 10); \
+    mpf_clear((variable_name))
+
+struct UI_Config {
     int screen_height;
     int screen_width;
     const char * _Nonnull window_name;
 };
 
-typedef struct Button Button;
-typedef bool (*button_callback)(struct Button * _Nonnull _In _Out);
+struct Player {
+    mpf_t experience;
+};
 
-struct Button type {
-    struct Display type {
+typedef struct Button Button;
+typedef bool (*button_callback)(
+    struct Button * _Nonnull _In _Out,
+    struct Player * _Nonnull _In _Out
+);
+
+struct Button {
+    struct Display {
         char * _Nonnull text;
         char * _Nullable afix;
         char * _Nullable prefix;
         char * _Nullable sufix;
     } display;
-    struct Style type {
+    struct Style {
         Vector2 position;
         float width;
         float height;
@@ -60,61 +79,106 @@ struct Button type {
         int border_width;
         Color border_color;
     } style;
-    struct Callbacks type {
+    struct Animations {
+        float color_fade_furation;
+    } animations;
+    struct Callbacks {
         button_callback on_idle;
         button_callback on_hover;
+        button_callback on_able_to_buy;
         button_callback on_click;
     } callbacks;
 };
 
+bool on_idle(
+    struct Button * _Nonnull _In _Out button,
+    struct Player * _Nonnull _In _Out player
+) {
+    (void)player;
+    return !CheckCollisionPointRec(
+        GetTouchPosition(0),
+        (struct Rectangle) {
+            .x = button->style.position.x,
+            .y = button->style.position.y,
+            .width = button->style.width,
+            .height = button->style.height
+    });
+}
 
-struct Result simple_button(struct Button button) proc {
-    struct Result result = set_result(OK, NULL, NULL);
-    struct Vector3 btn_color_hsv = ColorToHSV(button.style.color);
-    struct Rectangle area = {
-        .x = button.style.position.x,
-        .y = button.style.position.y,
-        .width = button.style.width,
-        .height = button.style.height,
-    };
+bool on_hover(
+    struct Button * _Nonnull _In _Out button,
+    struct Player * _Nonnull _In _Out player
+) {
+    (void)button;
+    (void)player;
+    return !IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
+}
 
-    // Iddle button
-    if (!CheckCollisionPointRec(GetTouchPosition(0), area)) {
-        btn_color_hsv.z *= 0.5f;
-        goto render_button;
-    }
+bool on_able_to_buy(
+    struct Button * _Nonnull _In _Out button,
+    struct Player * _Nonnull _In _Out player
+) {
+    (void)button;
+    new_mpf(cost, "1.0");
+    new_mpf(tmp, "100.0");
 
-    // Hover button
-    if (!IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-        btn_color_hsv.z *= 0.6f;
-        goto render_button;
-    }
-
-    // TODO: Implement onclick callback
-
-render_button:
-    button.style.color = ColorFromHSV(btn_color_hsv.x, btn_color_hsv.y, btn_color_hsv.z);
-
-    int text_width = MeasureText(button.display.text, button.style.text_size);
-    Rectangle border_area = {
-        .x = button.style.position.x - button.style.border_width,
-        .y = button.style.position.y - button.style.border_width,
-        .width = button.style.width + 2 * button.style.border_width,
-        .height = button.style.height + 2 * button.style.border_width,
-    };
-
-    DrawRectangleRounded(border_area, 0.2f, 30, button.style.border_color);
-    DrawRectangleRounded(area, 0.2f, 20, button.style.color);
-    DrawText(button.display.text,
-             button.style.position.x + (button.style.width - text_width) / 2,
-             button.style.position.y + (button.style.height - button.style.text_size) / 2,
-             button.style.text_size,
-             button.style.text_color);
+    bool result = mpf_cmp(player->experience, cost) >= 0;
+    mpf_clears(cost, tmp, NULL);
 
     return result;
 }
 
-struct Result main_game_entry(void) proc {
+struct Result simple_button(
+    struct Button * _Nonnull _In _Out button,
+    struct Player * _Nonnull _In _Out player
+) {
+    struct Result result = set_result(OK, NULL, NULL);
+    struct Vector3 btn_color_hsv = ColorToHSV(button->style.color);
+
+    // Iddle state
+    if (button->callbacks.on_idle(button, player)) {
+        btn_color_hsv.z = button->callbacks.on_able_to_buy(button, player) ? 0.8f : 0.5f;
+        goto render_button;
+    }
+
+    // Hover state
+    if (button->callbacks.on_hover(button, player)) {
+        btn_color_hsv.z = button->callbacks.on_able_to_buy(button, player) ? 0.9f : 0.6f;
+        goto render_button;
+    }
+
+    // TODO: Implement onclick state
+
+render_button:
+    button->style.color = ColorFromHSV(btn_color_hsv.x, btn_color_hsv.y, btn_color_hsv.z);
+
+    int text_width = MeasureText(button->display.text, button->style.text_size);
+    Rectangle border_area = {
+        .x = button->style.position.x - button->style.border_width,
+        .y = button->style.position.y - button->style.border_width,
+        .width = button->style.width + 2 * button->style.border_width,
+        .height = button->style.height + 2 * button->style.border_width,
+    };
+
+    DrawRectangleRounded(border_area, 0.2f, 30, button->style.border_color);
+    DrawRectangleRounded(
+        (struct Rectangle) {
+            .x = button->style.position.x,
+            .y = button->style.position.y,
+            .width = button->style.width,
+            .height = button->style.height,
+        }, 0.2f, 20, button->style.color);
+
+    DrawText(button->display.text,
+             button->style.position.x + (button->style.width - text_width) / 2,
+             button->style.position.y + (button->style.height - button->style.text_size) / 2,
+             button->style.text_size,
+             button->style.text_color);
+
+    return result;
+}
+
+struct Result main_game_entry(void) {
     struct Result result = set_result(OK, NULL, NULL);
     struct UI_Config ui_config = {1200, 800, "Infinitium RPG - Incremental Game"};
 
@@ -135,21 +199,41 @@ struct Result main_game_entry(void) proc {
             .border_color = BLACK,
             .border_width = 4.0f,
         },
+        .callbacks = {
+            .on_idle = on_idle,
+            .on_hover = on_hover,
+            .on_able_to_buy = on_able_to_buy,
+        },
     };
 
+    struct Player player;
+    new_mpf_from_struct(player.experience, "0.0");
+    float passed_time = 0.0f;
+    const int max_length = 1024;
+    char experience_str[max_length];
+    sprintf(experience_str, "0.00");
+
     InitWindow(ui_config.screen_height, ui_config.screen_width, ui_config.window_name);
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        simple_button(button);
-        EndDrawing();
-    }
+        start_renderer(!WindowShouldClose());
+            passed_time += GetFrameTime();
+            if (passed_time >= 1.0f) {
+                mpf_add_ui(player.experience, player.experience, 1);
+                gmp_snprintf(experience_str, max_length, "%.*Ff", 2, player.experience);
+                passed_time = 0.0f;
+            }
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText(experience_str, 300, 300, 30, BLACK);
+            simple_button(&button, &player);
+            EndDrawing();
+        end_renderer();
     CloseWindow();
 
+    clear_mpf(player.experience);
     return result;
 }
 
-int main() proc {
+int main() {
     struct Result result = main_game_entry();
     return get_result(result);
 }
